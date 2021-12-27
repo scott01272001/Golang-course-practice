5749101671fa39ce0042509ad26290cb87d57c2c
江炘韋 <scott01272001@gmail.com>
2021-12-28 00:23:36 +0800

    lesson-21-pass-slice-as-function-args

--- section-3/lesson-21-pass-slice-as-function-args/main.go
+++ section-3/lesson-21-pass-slice-as-function-args/main.go
@@ -8,15 +8,21 @@ func modify(foo []string) {
 }
 
 func addValue(foo []string) {
+	// 如果slice的cap不夠的話, append會回傳原本的array＊２
+	// 新array的cap會是原本的兩倍
 	foo = append(foo, "d")
-	fmt.Println("modify foo:", foo, " len:", len(foo), "cap:", cap(foo))
+	fmt.Println("modify foo:", foo)
+}
+
+func addValueByRef(foo *[]string) {
+	*foo = append(*foo, "e")
 }
 
 func main() {
-	foo := make([]string, 3)
+	foo := make([]string, 2, 4)
 	foo[0] = "a"
 	foo[1] = "b"
-	fmt.Println("len:", len(foo), "cap:", cap(foo))
+	fmt.Println("len:", len(foo))
 
 	fmt.Println("before foo:", foo)
 	modify(foo)
@@ -25,4 +31,18 @@ func main() {
 	fmt.Println("before foo:", foo)
 	addValue(foo)
 	fmt.Println("after foo:", foo)
+
+	foo2 := make([]string, 2, 3)
+	foo2[0] = "a"
+	foo2[1] = "b"
+
+	// foo2 cap 還夠, 不會複製新的ARRAY
+	fmt.Printf("%p \n", &foo2[0])
+	addValueByRef(&foo2)
+	fmt.Printf("%p \n", &foo2[0])
+
+	// foo2 cap 滿了, Slice pointer到新的array
+	fmt.Printf("%p \n", &foo2[0])
+	addValueByRef(&foo2)
+	fmt.Printf("%p \n", &foo2[0])
 }
