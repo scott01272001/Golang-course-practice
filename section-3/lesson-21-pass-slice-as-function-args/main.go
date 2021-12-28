1e1f076ce7e43621e98dfa4e8ab70120e4a93ea7
江炘韋 <scott01272001@gmail.com>
2021-12-28 20:56:28 +0800

    lesson-21-pass-slice-as-function-args

--- section-3/lesson-21-pass-slice-as-function-args/main.go
+++ section-3/lesson-21-pass-slice-as-function-args/main.go
@@ -45,4 +45,32 @@ func main() {
 	fmt.Printf("%p \n", &foo2[0])
 	addValueByRef(&foo2)
 	fmt.Printf("%p \n", &foo2[0])
+
+	foo3 := []string{"a", "b"}
+	fmt.Printf("foo3[0] address: %p \n", &foo3[0])
+
+	// bar是一個pointer到foo3 array[0]的slice
+	// 所以修改bar foo3也會被修改, 因為實際上是pointer到相同的array
+	bar := foo3[:1]
+	fmt.Println("bar:", bar)
+	fmt.Printf("bar[0] address: %p \n", &bar[0])
+
+	s1 := append(bar, "c")
+	fmt.Println("foo3:", foo3)
+	fmt.Printf("s1 address: %p \n", &s1)
+	fmt.Printf("bar address: %p \n", &bar)
+
+	fmt.Println("s1:", s1)
+	fmt.Printf("s1[0] address: %p \n", &s1[0])
+
+	s2 := append(bar, "d")
+	fmt.Println("foo3:", foo3)
+	fmt.Println("s2:", s2)
+
+	// 超過foo3的capacity, append會回傳一個新的array
+	s3 := append(bar, "e", "f")
+	fmt.Println("foo3:", foo3)
+	fmt.Println("s3:", s3)
+	fmt.Printf("foo3[0] address: %p \n", &foo3[0])
+	fmt.Printf("s3[0] address: %p \n", &s3[0])
 }
